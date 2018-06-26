ba8b6ece2b68de7526fa5eacf1392785fd878906
李威 <wei.li@administratordeMacBook-Pro.local>
2018-06-26 17:19:56 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-1 col-xs-offet-11">
+              <h1>hello world</h1>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
