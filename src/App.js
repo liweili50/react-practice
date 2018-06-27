ccaa867fec748f2fd9f980ae2bc748b424f37b55
liweili50 <wei.li@administratordeMacBook-Pro.local>
2018-06-27 11:04:44 +0800

    组件的state属性

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
         </div>
         <div className="row">
           <div className="col-xs-1 col-xs-offet-11">
-            <Home name={'Max'} age={12} user={user}>
+            <Home name={'Max'} initalAge={12} user={user}>
             <p>i am child</p>
             </Home>
           </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-export default class Home extends Component {
+export default class Home extends Component { 
+  constructor (props) {
+    super(props);
+    this.state = {
+      age: props.initalAge
+    }
+    this.onMarkOlder = this.onMarkOlder.bind(this)
+  }
+  onMarkOlder() {
+    this.setState({
+      age: this.state.age+3
+    })
+  }
   render() {
     console.log(this.props)
     return (
         <div className="container">
           <div className="row">
             <div className="col-xs-1 col-xs-offet-11">
-              <div>your name is {this.props.name}，age{this.props.age}</div>
-              <div>
-                <h5>hobbies</h5>
-                <ul>
-                  {this.props.user.hobbies.map(function (hobby,i) {
-                    return <li key = {i}>{hobby}</li>
-                  })}
-                </ul>
-              </div>
-              <div>
-                {this.props.children}
-              </div>
+              <div>your name is {this.props.name}，age{this.state.age}</div>
+              <button onClick={this.onMarkOlder} className="btn btn-primary">Make me older</button>
             </div>
           </div>
         </div>
@@ -29,5 +31,6 @@ export default class Home extends Component {
 Home.propTypes = {
   name: PropTypes.string,
   age: PropTypes.number,
-  user: PropTypes.object
+  user: PropTypes.object,
+  children:PropTypes.element.isRequired
 };
