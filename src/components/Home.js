329641ee57b80429b78e8818af2bd0beebfa97f6
liweili50 <wei.li@administratordeMacBook-Pro.local>
2018-06-27 15:24:29 +0800

    双向绑定

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,13 @@ export default class Home extends Component {
     super(props);
     this.state = {
       age: props.initalAge,
-      linkName: 'I am a new Name'
+      linkName: 'I am a new Name',
+      inputValue: ''
     }
     this.onMarkOlder = this.onMarkOlder.bind(this);
     this.handleGreet = this.handleGreet.bind(this);
-    this.handleChangeName = this.handleChangeName.bind(this)
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeInput = this.handleChangeInput.bind(this)
   }
   onMarkOlder() {
     this.setState({
@@ -22,6 +24,10 @@ export default class Home extends Component {
   handleChangeName() {
     this.props.changeName(this.state.linkName)
   }
+  handleChangeInput(event) {
+    this.setState({inputValue: event.target.value});
+    this.props.changeName(this.state.inputValue)
+  }
   render() {
     console.log(this.props)
     return (
@@ -31,9 +37,12 @@ export default class Home extends Component {
               <div>your name is {this.props.name}，age{this.state.age}</div>
               <button onClick={this.onMarkOlder} className="btn btn-primary">Make me older</button>
               <hr />
-              <button onClick={this.handleGreet} className="btn btn-primary">Make me older</button>
+              <button onClick={this.handleGreet} className="btn btn-primary">greet</button>
               <hr />
               <button onClick={this.handleChangeName} className="btn btn-primary">Change Name</button>
+              <hr />
+              <p>{this.state.inputValue}</p>
+              <label> name: <input type="text" value={this.state.inputValue} onChange={this.handleChangeInput} /></label>
             </div>
           </div>
         </div>
